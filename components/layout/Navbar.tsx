a4b2d6103b90bc629e346d8bc93ea16108fa32f7
Johnny Hall <jbhall4291@gmail.com>
2025-10-06 19:22:06 +0100

    update logo, add skeleton loaders for library pages

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,17 +2,25 @@
 
 import Link from 'next/link';
 import { useGameContext } from '@/lib/GameContext';
+import { BETA_EVENT } from '@/components/BetaModal';
+
 // optional: import { useEffect } from 'react';
 
 export default function Navbar() {
     const { gameCount } = useGameContext();
 
     return (
-        <nav className="bg-black text-white p-4 shadow">
+        <nav className="bg-[#1d1d1f] text-white p-4 shadow">
             <div className="max-w-7xl mx-auto flex items-center justify-between">
                 <Link href="/">
-                    <img src="/logo-large.jpg" className="h-24" alt="Logo" />
+                    <img src="/logo.png" className="h-24" alt="Logo" />
                 </Link>
+
+
+                <button onClick={() => window.dispatchEvent(new CustomEvent(BETA_EVENT))}
+                    className="cursor-pointer bg-white text-red-600 border-2 font-bold border-red-600 px-3 py-1 rounded-full">
+                    BETA • In Development
+                </button>
 
                 <ul className="flex space-x-6">
                     <li className="relative">
