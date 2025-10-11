63039e45d6d7b09dbe5692060b4c1870cad40c6c
Johnny Hall <jbhall4291@gmail.com>
2025-10-11 18:52:06 +0100

    add placeholder profile page for user

---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function LibraryPage() {
+
+    return (
+        <main className="relative p-6 font-sans  min-h-screen ">
+            user profile page
+        </main>
+    );
+}
