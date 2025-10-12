88b249261119d8074bcfe34dcc46871d735694b3
Johnny Hall <jbhall4291@gmail.com>
2025-10-12 17:45:55 +0100

    tweak style

--- components/layout/BrandLogo.tsx
+++ components/layout/BrandLogo.tsx
@@ -19,12 +19,12 @@ export default function BrandLogo({ shimmer = true, }: BrandLogoProps) {
             className={cn(
                 silkscreen.className,
                 "relative inline-flex items-center gap-3 w-fit cursor-default select-none",
-                "rounded-2xl px-5 pt-2 pb-2.5 bg-green-500 text-white shadow-[0_6px_24px_rgba(16, 185, 129, .25)]",
+                "rounded-2xl px-7 pt-2 pb-2.5 bg-green-500 text-white shadow-[0_6px_24px_rgba(16, 185, 129, .25)]",
                 shimmer && "shimmer",
 
             )}
         >
-            <Crown strokeWidth={2.5} className="text-[#ffdd00] h-5 w-5 md:h-11 md:w-11" />
+            <Crown strokeWidth={2.5} className="text-[#ffdd00] h-5.5 w-5.5 md:h-11 md:w-11" />
             <span className="tracking-tight text-lg md:text-5xl uppercase">
                 Completed It Mate
             </span>
