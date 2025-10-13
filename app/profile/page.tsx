7b52878f6b3b79337c8ad1ce3cb9efd1199d373f
Johnny Hall <jbhall4291@gmail.com>
2025-10-13 11:56:45 +0100

    add placeholder content

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,20 @@
 'use client';
 
+import UsernameSetter from "@/components/user/UsernameSetter";
+
 export default function LibraryPage() {
 
     return (
         <main className="relative p-6 font-sans  min-h-screen ">
             user profile page
+
+            <div>link to my games for dash board, or just show stats here too with link there to see more</div>
+
+            <div>Member since: xxxx </div>
+
+            <UsernameSetter />
+
+
         </main>
     );
 }
