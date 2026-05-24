c7c9389cf6b18fa6bddd98b95a0f927f69a7b6dc
Johnny Hall <jbhall4291@gmail.com>
2026-05-24 16:53:03 +0100

    add placeholders for loading/missing game art

--- components/game/GameCard.tsx
+++ components/game/GameCard.tsx
@@ -64,6 +64,7 @@ function GameCard({
         >
             {/* Full-card clickable layer */}
             <Link
+                prefetch={false}
                 href={`/games/${game.id}`}
                 aria-label={`Open ${game.title}`}
                 className="absolute inset-0 z-0"
